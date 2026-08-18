9ebfab1cba426902594113051c36070698a50ee7
Rishon100 <rishonvijay@gmail.com>
2026-08-18 22:40:28 +0530

    Complete risk-weighted toy experiment

--- src/toy_graph.py
+++ src/toy_graph.py
@@ -3,17 +3,19 @@ import networkx as nx
 graph = nx.Graph()
 
 # Path 1
-graph.add_edge("START", "A")
-graph.add_edge("A", "B")
-graph.add_edge("B", "TARGET")
+graph.add_edge("START", "A",weight = 5)
+graph.add_edge("A", "B",weight = 5)
+graph.add_edge("B", "TARGET",weight = 5)
 
 # Path 2
-graph.add_edge("START", "C")
-graph.add_edge("C", "D")
-graph.add_edge("D", "E")
-graph.add_edge("E", "TARGET")
+graph.add_edge("START", "C",weight = 1)
+graph.add_edge("C", "D",weight = 1)
+graph.add_edge("D", "E",weight = 1)
+graph.add_edge("E", "TARGET",weight = 1)
 
-path = nx.shortest_path(graph, "START", "TARGET")
+path = nx.shortest_path(graph, "START", "TARGET",weight='weight')
+total_cost = nx.path_weight(graph, path, weight="weight")
 
-print("Shortest path:", path)
+print("Risk-weighted path:", path)
 print("Number of hops:", len(path) - 1)
+print("Total detection cost:", total_cost)
